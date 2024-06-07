3c9f182de88c1d743dc1574bcc762fbfd0aca2e3
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-07 13:48:14 -0600

    feat: merchant new discount page

--- spec/features/discounts/new_spec.rb
+++ spec/features/discounts/new_spec.rb
@@ -8,12 +8,23 @@ RSpec.describe 'discounts new page' do
   it 'can create a new discount' do
     visit new_merchant_discount_path(@merchant)
 
-    fill_in 'Percentage', with: 23
+    fill_in 'Percentage', with: 0.23
     fill_in 'Quantity Threshold', with: 17
 
     click_button 'Create Discount'
 
     expect(current_path).to eq(merchant_discounts_path(@merchant))
-    expect(page).to have_content('23% off 17 items or more')
+    expect(page).to have_content('23.0% off 17 items or more')
+  end
+
+  it 'cannot create a discount without required information' do
+    visit new_merchant_discount_path(@merchant)
+
+    fill_in 'Percentage', with: 0.23
+
+    click_button 'Create Discount'
+
+    expect(page).to have_content('Discount not created: Required information missing.')
+    expect(current_path).to eq(new_merchant_discount_path(@merchant))
   end
 end
