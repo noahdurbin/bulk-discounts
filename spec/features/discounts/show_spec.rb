71bbddf4c43f298e48afbc8aafd178daa92b0995
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-06-07 18:27:44 -0600

    test: test discount show page

---
+++ spec/features/discounts/show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'discount show page' do
+  before :each do
+    @merchant1 = Merchant.create!(name: 'Hair Care')
+    @discount1 = @merchant1.discounts.create!(percentage: 0.1, quantity_threshold: 10)
+  end
+
+  describe 'as a merchant' do
+    it 'displays a discounts ID' do
+      visit merchant_discount_path(@merchant1, @discount1)
+
+      expect(page).to have_content("Discount #{@discount1.id}")
+    end
+
+    it 'displays a discounts percentage off' do
+      visit merchant_discount_path(@merchant1, @discount1)
+
+      expect(page).to have_content('10.0% off')
+    end
+
+    it 'displays a discounts item threshold for a discount' do
+      visit merchant_discount_path(@merchant1, @discount1)
+
+      expect(page).to have_content('10 items or more')
+    end
+  end
+end
